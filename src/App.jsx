ab9f9ff895d055ff6e716eb47830a55a968930a0
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-11-06 12:21:30 +0530

    nav home and header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home.jsx/Home'
 
 const App = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
   )
 }
 
