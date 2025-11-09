9cbdaba49c57b042f2923aa44f89d968af57dda7
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-11-09 21:14:23 +0530

    90% completed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://www.shutterstock.com/image-vector/people-working-office-coworking-space-600nw-520160074.jpg"
                             alt="image"
                         />
                     </div>
